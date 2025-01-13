bb5f4a3ed81e0d78229350282519e52cfdc7bdda
KnollElias <knoll.elias@icloud.com>
2025-01-13 17:12:58 +0100

    WErked on multi

---
+++ splitmartin/multi.js
@@ -0,0 +1,46 @@
+balances = [100, 100, 100, 100, 100, 100, 100, 100];
+balancesLastRound = [100, 100, 100, 100, 100, 100, 100, 100];
+nodeCount = 8;
+nodeActive = nodeCount; // so the first iteration starts with one, could also set it to "1" but meh
+
+riskInitial = 1;
+risk = [riskInitial, riskInitial, riskInitial, riskInitial, riskInitial, riskInitial, riskInitial, riskInitial];
+loss = 1;
+gain = 0.9;
+riskMulityplier = 2;
+
+iterations = 16 * 2;
+
+function upgradeNode() {
+    nodeActive = nodeActive == nodeCount ? 1 : nodeActive + 1;
+};
+
+function getRandomBool() {
+    return Math.random() >= 0.5;
+}
+
+for (i = 0; i < iterations; i++) {
+    upgradeNode();
+    winThisRound = getRandomBool();
+
+    // raise risk if lost last round
+    if (balances[nodeActive - 1] < balancesLastRound[nodeActive - 1]) { // lostLastRound
+        risk[nodeActive - 1] *= riskMulityplier;
+    } else {
+        risk[nodeActive - 1] = riskInitial;
+    }
+
+    // update last round balances based on current risk
+    if (winThisRound) {
+        balances[nodeActive - 1] += risk[nodeActive - 1] * gain;
+    } else {
+        balances[nodeActive - 1] -= risk[nodeActive - 1] * loss;
+    }
+
+    if (((i+1) / nodeCount) == 0) {
+        console.log(" asd ");
+    }
+    console.log("Iteration: " + i + " Node: " + nodeActive + " Win: " + winThisRound + " Risk: " + risk[nodeActive - 1]);
+}
+
+console.log(balances);
