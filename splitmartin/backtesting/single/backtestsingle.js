d03a410faf3371f443e5961f7f4e2a8f054ce0a7
KnollElias <knoll.elias@icloud.com>
2025-01-16 18:33:01 +0100

    rework benchmark to make fuckup calculation

--- splitmartin/backtesting/single/backtestsingle.js
+++ splitmartin/backtesting/single/backtestsingle.js
@@ -1,7 +1,11 @@
+// @2021 reinvented the sma
+// @2023 perfectionized martingale nodes
+// @2025 realworld application with options in papertrading
+
 const fs = require('fs');
 
-const inputPath = "/home/main/Documents/GitHub/MEM-Trading/splitmartin/btc-usd-edited.csv";
-const outputPath = "/home/main/Documents/GitHub/MEM-Trading/splitmartin/backtesting/singlebacktest.csv";
+const inputPath = "/home/main/Documents/GitHub/MEM-Trading/splitmartin/btc-usd-edited-test.csv";
+const outputPath = "/home/main/Desktop/singlebacktest.csv";
 
 // Load dataset
 const data = fs.readFileSync(inputPath, 'utf8');
@@ -18,24 +22,51 @@ const priceData = rows.map(row => {
 }).filter(row => !isNaN(row.price) && !isNaN(row.sma)); // Filter out rows with missing SMA
 
 // Initialize balances and risk variables
-let balance = 12.5;
+let balance = 100;
 let balanceLastRound = balance;
-let risk = 20; // Start with initial risk
+let risk = 1; // Start with initial risk
 
-const riskInitial = 20;
+const riskInitial = 1;
 const loss = 1;
 const gain = 0.9;
 const riskMultiplier = 2;
 
 // Write header to output file
-fs.writeFileSync(outputPath,  'date,price,sma,trend,risk,balance\n');
+fs.writeFileSync(outputPath,  'date,price,sma,trend,risk,balance,trade\n');
+console.log("grüezi");
 
 // Main backtesting loop
 priceData.forEach((row, i) => {
     const { snapped_at, price, sma } = row;
+    const lastPrice = priceData[i - 1]?.price;
+    const lastSma = priceData[i - 1]?.price;
+
+    // get trend
+    let lastTrend = 0;
+    if (priceData[i - 1]?.price >= priceData[i - 1]?.sma) {
+        lastTrend = 1;
+    } else {
+        lastTrend = -1;
+    }
+console.log(`${i},${price},${sma},${lastTrend},${risk},${balance}`)
+    // get outcome
+    if (lastTrend !== 0) { // uptrend
+        if (lastTrend === 1 && price > lastPrice || lastTrend === -1 && price < lastPrice)
+        {
+            // won action
+            console.warn("won")
+        } else { // downtrend
+            // lost action
+            console.error("lost")
+        }
+    
+    }
+
+
+    
+
 
     // Determine trend and outcome
-    let trend = '';
     let winThisRound = false;
 
     if (price > sma) {
@@ -69,9 +100,9 @@ priceData.forEach((row, i) => {
     balanceLastRound = balance;
 
     // Output to CSV
-    fs.appendFileSync(
-        outputPath, `${snapped_at},${price},${sma},${trend},${risk},${balance}\n`
-    );
+    // fs.appendFileSync(
+    //     outputPath, `${snapped_at},${price},${sma},${trend},${risk},${balance}\n`
+    // );
 });
 
 console.log('Backtest complete. Results saved to backtest.csv');
