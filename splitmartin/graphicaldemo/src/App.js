c6e5621bed0e2dc02fdf4d6a733045aecf2917ef
KnollElias <knoll.elias@icloud.com>
2025-01-22 00:04:54 +0100

    Tried to continue on the visual multimartingale scale project

--- splitmartin/graphicaldemo/src/App.js
+++ splitmartin/graphicaldemo/src/App.js
@@ -1,37 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const PontLength = 3; // How many times i can fail
+  const PontLength = 3; // How many times one node can fail
   const NodeCount = 16; // How many different Scales there are
 
-  const [scales, setState] = useState([
-    {
-      index: 1,
-      size: 3,
-      state: 0,
-    }
-  ]);
+  const [scales, setScales] = useState([]);
+
+  // Function to initialize scales
+  const initializeScales = () => {
+    const newScales = Array.from({ length: NodeCount }, (_, index) => ({
+      index: index + 1,
+      size: PontLength,
+      state: 0, // Default state
+    }));
+    setScales(newScales);
+  };
+
+  // Run the initializeScales function once on component mount
+  useEffect(() => {
+    initializeScales();
+  }, []);
 
   const plotScales = (scales) => {
-    let output = "";
-    for (let scale of scales) {
-      for (let riskLevel = 0; riskLevel < scale.size; riskLevel++) {
-        console.warn("risk level_: " + riskLevel)
-        if (riskLevel > scale.state) {
-          output += `<div>green</div>`;
-        }
-      }
-      output += `<div>yellow</div>`;
-    }
-    return output;
-  }
+    return scales.map((scale, scaleIndex) => (
+      <div key={scaleIndex} className="item">
+        <div>
+          Node: {scaleIndex+1}
+        </div>
+        {Array.from({ length: scale.size }).map((_, riskLevel) => (
+          <div
+            key={riskLevel}
+            className={`${riskLevel > (scale.state-1) ? 'free' : 'used'}`}
+          >
+            {riskLevel > (scale.state-1) ? `Ok: ${((riskLevel+1)*(riskLevel+2))}`: "Risk"}
+          </div>
+        ))}
+      </div>
+    ));
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <div className='container'>
+        <div className="container">
+          <div className='dash'>Dashboard
+            <div>Current Working Node:</div>
+            <div>Last Outcome:</div>
+            <div>Next Outcome:</div>
+            <div>Balance:</div>
+            <div>Unrealized Loss:</div>
+          </div>
           {plotScales(scales)}
         </div>
       </header>
