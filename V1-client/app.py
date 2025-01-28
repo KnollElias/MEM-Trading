69b6c9d10257be6b6b9bfcd12e789850045e9b7a
KnollElias <knoll.elias@icloud.com>
2025-01-28 08:14:08 +0100

    Finished winaction

--- V1-client/app.py
+++ V1-client/app.py
@@ -12,24 +12,35 @@ def increment_nodes(currentnode, statefile):
     # increase the current an modulo % the nodes length
     # increase the age
 
+def reset_scales(node_index, statefile, filepath=None):
+    # Ensure node_index is valid
+    if 0 <= node_index < len(statefile["nodes"]):
+        statefile["nodes"][node_index]["scales"] = [0] * len(statefile["nodes"][node_index]["scales"])
+        write_statefile("nodes", statefile["nodes"], filepath)
+    else:
+        print(f"Invalid node index: {node_index}.")
+
+
 def iteration():
     lastoutcome = read_outcome() # bool
     statefile = read_statefile() # json
 
+    initialrisk = statefile["initial_risk"]
     currentnode = statefile["current_node"]
     lastnode = statefile["last_node"]
     balance = statefile["balance"]
 
     increment_nodes(currentnode, statefile)
 
+    currentrisk = statefile["nodes"][currentnode]["state"]
+    
+    if lastoutcome:
+        reset_scales(currentnode, statefile)
+    
+        statefile["nodes"][currentnode]["state"] = 0
+        write_statefile("nodes", statefile["nodes"])
 
 
-    # if lastoutcome: # won
-    #     statefile
-
-
-    # calculate outcoem of last
-    # get node index
     # get current risk
     # write the position as open
     # so the nodeindex must be -1 bevause we are workign wiht a starting at 0 array here, then the state can be read
@@ -46,8 +57,4 @@ def iteration():
 # notify a discord chanel or so
 
 iteration()
-iteration()
-iteration()
-iteration()
-iteration()
-iteration()
+
