d880f780cdf1b3e6ba1ca8f7843c43d6e8aea8f0
KnollElias <knoll.elias@icloud.com>
2025-01-28 19:10:05 +0100

    Made system generally work, need to make it able to backtest

--- V1-client/app.py
+++ V1-client/app.py
@@ -1,16 +1,18 @@
 from modules.logger import log
 from modules.statefile import show_statefile, read_statefile, write_statefile
+from modules.trader import open_new_trade
+from modules.messanger import notify_message
 
 def read_outcome():
     return False
 
 def increment_nodes(currentnode, statefile):
-    write_statefile("last_node", currentnode)
-    write_statefile("current_node", (currentnode + 1) % len(statefile["nodes"]))
-    write_statefile("age", statefile["age"] + 1)
     # make the last the current
+    write_statefile("last_node", currentnode)
     # increase the current an modulo % the nodes length
+    write_statefile("current_node", (currentnode + 1) % len(statefile["nodes"]))
     # increase the age
+    write_statefile("age", statefile["age"] + 1)
 
 def reset_scale(node_index, statefile, filepath=None):
     # Ensure node_index is valid
@@ -23,9 +25,12 @@ def reset_scale(node_index, statefile, filepath=None):
 def brew_scale(oldscale, newstate):
     if newstate == 1:
         oldscale[0] = 1
-        print(oldscale)
         return oldscale
-
+    if newstate >= 2:
+        position = newstate - 1
+        risk = 2 ** (newstate - 1)
+        oldscale[position] = risk
+        return oldscale
 
 def iteration():
     lastoutcome = read_outcome() # bool
@@ -40,38 +45,15 @@ def iteration():
     
     if lastoutcome:
         reset_scale(currentnode, statefile)
-    
         statefile["nodes"][currentnode]["state"] = 0
+
         write_statefile("nodes", statefile["nodes"])
     if lastoutcome == False:
         statefile["nodes"][currentnode]["state"] += 1
-        newscale = brew_scale(statefile["nodes"][currentnode]["scale"], statefile["nodes"][currentnode]["state"])
-aksjldföhahsdfi
+        statefile["nodes"][currentnode]["scale"] = brew_scale(statefile["nodes"][currentnode]["scale"], statefile["nodes"][currentnode]["state"])
 
         write_statefile("nodes", statefile["nodes"])
 
-        newscale = statefile["nodes"][currentnode]["scale"]
-        
-        print("scale : ", newscale)
-        print("current state : ", statefile["nodes"][currentnode]["state"])
-        # how many times fuckedup ** 2
-        statefile["nodes"][currentnode]["state"]
-
-
-    # get current risk
-    # write the position as open
-    # so the nodeindex must be -1 bevause we are workign wiht a starting at 0 array here, then the state can be read
-    # print(statefile["nodes"][0]["state"])
-    # print(lastoutcome)
-    print("was for the currentNode: ", currentnode)
-    # print("lastnode: ", lastnode)
-
-
-# read statefile
-
-# get next nodeindex
-
-# notify a discord chanel or so
-
+    # open_new_trade(read_statefile()) # read statefile again after updates
+    # notify_message(read_statefile()) # put the data to a discord chat for info
 iteration()
-
---
+++ V1-client/modules/trader.py
@@ -0,0 +1,2 @@
+def open_new_trade(statefile):
+    print("Opening new trade... with following information:", statefile)
